2aecd8404f3a6bf211664b2ce0067fcd12e36037
cepheus3 <nebiyouelias96@gmail.com>
2023-05-21 05:47:00 -0400

    buy stock

--- src/components/ChartTwo.js
+++ src/components/ChartTwo.js
@@ -77,96 +77,151 @@ const ChartTwo = () => {
   });
 
   const [enabled, setEnabled] = useState(false);
+  const [buy, setBuy] = useState(false);
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
-      <div className="mb-4 justify-between gap-4 sm:flex">
-        <div>
-          <h4 className="text-xl font-semibold text-black dark:text-white">
-            stock bay/sell
-          </h4>
-        </div>
-        <div>
-          <div className="relative z-20 inline-block">
-            <select
-              name="#"
-              id="#"
-              className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
-            >
-              <option value="">BAY</option>
-              <option value="">SEL</option>
-            </select>
-            <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
-              <svg
-                width="10"
-                height="6"
-                viewBox="0 0 10 6"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.47072 1.08816C0.47072 1.02932 0.500141 0.955772 0.54427 0.911642C0.647241 0.808672 0.809051 0.808672 0.912022 0.896932L4.85431 4.60386C4.92785 4.67741 5.06025 4.67741 5.14851 4.60386L9.09079 0.896932C9.19376 0.793962 9.35557 0.808672 9.45854 0.911642C9.56151 1.01461 9.5468 1.17642 9.44383 1.27939L5.50155 4.98632C5.22206 5.23639 4.78076 5.23639 4.51598 4.98632L0.558981 1.27939C0.50014 1.22055 0.47072 1.16171 0.47072 1.08816Z"
-                  fill="#637381"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.22659 0.546578L5.00141 4.09604L8.76422 0.557869C9.08459 0.244537 9.54201 0.329403 9.79139 0.578788C10.112 0.899434 10.0277 1.36122 9.77668 1.61224L9.76644 1.62248L5.81552 5.33722C5.36257 5.74249 4.6445 5.7544 4.19352 5.32924C4.19327 5.32901 4.19377 5.32948 4.19352 5.32924L0.225953 1.61241C0.102762 1.48922 -4.20186e-08 1.31674 -3.20269e-08 1.08816C-2.40601e-08 0.905899 0.0780105 0.712197 0.211421 0.578787C0.494701 0.295506 0.935574 0.297138 1.21836 0.539529L1.22659 0.546578ZM4.51598 4.98632C4.78076 5.23639 5.22206 5.23639 5.50155 4.98632L9.44383 1.27939C9.5468 1.17642 9.56151 1.01461 9.45854 0.911642C9.35557 0.808672 9.19376 0.793962 9.09079 0.896932L5.14851 4.60386C5.06025 4.67741 4.92785 4.67741 4.85431 4.60386L0.912022 0.896932C0.809051 0.808672 0.647241 0.808672 0.54427 0.911642C0.500141 0.955772 0.47072 1.02932 0.47072 1.08816C0.47072 1.16171 0.50014 1.22055 0.558981 1.27939L4.51598 4.98632Z"
-                  fill="#637381"
-                />
-              </svg>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark"></div>
-        <div className="flex flex-col gap-5.5 p-6.5">
-          <div>
-            <label className="mb-3 block text-black dark:text-white">
-              No of Stock
-            </label>
-            <input
-              type="number"
-              className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-            />
-          </div>
-
-          <div>
-            <label className="mb-3 block font-medium text-black dark:text-white">
-              Want insured?
-            </label>
+    <>
+      
+        <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+          <div className="mb-4 justify-between gap-4 sm:flex">
             <div>
-              <label
-                htmlFor="toggle1"
-                className="flex cursor-pointer select-none items-center"
-              >
-                <div className="relative">
-                  <input
-                    type="checkbox"
-                    id="toggle1"
-                    className="sr-only"
-                    onChange={() => {
-                      setEnabled(!enabled);
-                    }}
-                  />
-                  <div className="block h-8 w-14 rounded-full bg-meta-9 dark:bg-[#5A616B]"></div>
-                  <div
-                    className={`absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${
-                      enabled &&
-                      '!right-1 !translate-x-full !bg-primary dark:!bg-white'
-                    }`}
-                  ></div>
-                </div>
-              </label>
+              <h4 className="text-xl font-semibold text-black dark:text-white">
+                stock Buy/Sell
+              </h4>
+            </div>
+            <div>
+              <div className="relative z-20 inline-block">
+                <select
+                  onChange={() => {
+                    setBuy(!buy);
+                  }}
+                  name="#"
+                  id="#"
+                  className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
+                >
+                  <option value="">BUY</option>
+                  <option value="">SELL</option>
+                </select>
+                <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
+                  <svg
+                    width="10"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M0.47072 1.08816C0.47072 1.02932 0.500141 0.955772 0.54427 0.911642C0.647241 0.808672 0.809051 0.808672 0.912022 0.896932L4.85431 4.60386C4.92785 4.67741 5.06025 4.67741 5.14851 4.60386L9.09079 0.896932C9.19376 0.793962 9.35557 0.808672 9.45854 0.911642C9.56151 1.01461 9.5468 1.17642 9.44383 1.27939L5.50155 4.98632C5.22206 5.23639 4.78076 5.23639 4.51598 4.98632L0.558981 1.27939C0.50014 1.22055 0.47072 1.16171 0.47072 1.08816Z"
+                      fill="#637381"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M1.22659 0.546578L5.00141 4.09604L8.76422 0.557869C9.08459 0.244537 9.54201 0.329403 9.79139 0.578788C10.112 0.899434 10.0277 1.36122 9.77668 1.61224L9.76644 1.62248L5.81552 5.33722C5.36257 5.74249 4.6445 5.7544 4.19352 5.32924C4.19327 5.32901 4.19377 5.32948 4.19352 5.32924L0.225953 1.61241C0.102762 1.48922 -4.20186e-08 1.31674 -3.20269e-08 1.08816C-2.40601e-08 0.905899 0.0780105 0.712197 0.211421 0.578787C0.494701 0.295506 0.935574 0.297138 1.21836 0.539529L1.22659 0.546578ZM4.51598 4.98632C4.78076 5.23639 5.22206 5.23639 5.50155 4.98632L9.44383 1.27939C9.5468 1.17642 9.56151 1.01461 9.45854 0.911642C9.35557 0.808672 9.19376 0.793962 9.09079 0.896932L5.14851 4.60386C5.06025 4.67741 4.92785 4.67741 4.85431 4.60386L0.912022 0.896932C0.809051 0.808672 0.647241 0.808672 0.54427 0.911642C0.500141 0.955772 0.47072 1.02932 0.47072 1.08816C0.47072 1.16171 0.50014 1.22055 0.558981 1.27939L4.51598 4.98632Z"
+                      fill="#637381"
+                    />
+                  </svg>
+                </span>
+              </div>
             </div>
           </div>
+
+      {buy && 
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark"></div>
+      <div className="flex flex-col gap-5.5 p-6.5">
+        <div>
+          <label className="mb-3 block text-black dark:text-white">
+            No of Stock
+          </label>
+          <input
+            type="number"
+            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+          />
         </div>
-        <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
-          BUY Stock
+
+        
+        <div>
+          <label className="mb-3 block text-black dark:text-white">
+            Select Order Type
+          </label>
+          <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input">
+            <option value="">Market Order</option>
+            <option value="">Limit order</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <button className=" w-full justify-center rounded bg-primary p-3 font-medium text-gray">
+          SELL
         </button>
       </div>
+    </div>}
+
+
+
+    {!buy && 
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark"></div>
+    <div className="flex flex-col gap-5.5 p-6.5">
+      <div>
+        <label className="mb-3 block text-black dark:text-white">
+          No of Stock
+        </label>
+        <input
+          type="number"
+          className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+        />
+      </div>
+
+      <div>
+        <label className="mb-3 block font-medium text-black dark:text-white">
+          Apply insurance
+        </label>
+        <div>
+          <label
+            htmlFor="toggle1"
+            className="flex cursor-pointer select-none items-center"
+          >
+            <div className="relative">
+              <input
+                type="checkbox"
+                id="toggle1"
+                className="sr-only"
+                onChange={() => {
+                  setEnabled(!enabled);
+                }}
+              />
+              <div className="block h-8 w-14 rounded-full bg-meta-9 dark:bg-[#5A616B]"></div>
+              <div
+                className={`absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${
+                  enabled &&
+                  '!right-1 !translate-x-full !bg-primary dark:!bg-white'
+                }`}
+              ></div>
+            </div>
+          </label>
+        </div>
+      </div>
+      <div>
+        <label className="mb-3 block text-black dark:text-white">
+          Select Order Type
+        </label>
+        <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input">
+          <option value="">Market Order</option>
+          <option value="">Limit order</option>
+        </select>
+      </div>
     </div>
+    <div>
+      <button className=" w-full justify-center rounded bg-primary p-3 font-medium text-gray">
+        BUY
+      </button>
+    </div>
+  </div>}
+        </div>
+      
+      
+    </>
   );
 };
 
--- src/pages/Form/FormLayout.js
+++ src/pages/Form/FormLayout.js
@@ -19,25 +19,15 @@ const FormLayout = () => {
               <div className="p-6.5">
                 <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
                   <div className="w-full xl:w-1/2">
-                    <label className="mb-2.5 block text-black dark:text-white">
-                      First name
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Enter your first name"
-                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                    />
+                    <button className=" w-half justify-center rounded bg-primary  font-medium text-gray">
+                      BUY
+                    </button>
                   </div>
 
                   <div className="w-full xl:w-1/2">
-                    <label className="mb-2.5 block text-black dark:text-white">
-                      Last name
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Enter your last name"
-                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                    />
+                    <button className=" w-half justify-center rounded bg-primary  font-medium text-gray">
+                      BUY
+                    </button>
                   </div>
                 </div>
 
